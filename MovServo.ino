4b03fbd622c53fb8eeb2916fbbe16976521933e6
Michu117 <fabricio.ruiz@unl.edu.ec>
2024-06-18 00:24:13 -0500

    Update MovServo.ino

--- MovServo.ino
+++ MovServo.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-// Definimos los pines para los dos servomotores y las dos fotorresistencias
+// Definición de los pines para los dos servomotores y las dos fotorresistencias
 const int servoPin1 = 9;   // Pin para el servo de orientación
 const int servoPin2 = 10;  // Pin para el servo de inclinación
 const int ldrPin1 = A0;    // Pin para la fotorresistencia horizontal
